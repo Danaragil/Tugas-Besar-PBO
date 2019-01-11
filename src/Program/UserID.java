3a8b8c8770b47885ee5723b62097513a35644983
danaragil <danar.agil@student.uty.ac.id>
2019-01-11 09:28:07 +0700

    Tugas Besar
    Kelompok :
    1. Danar Agil Prasetyo (5170411264)
    2. Deddy Cahya Fitrianto (5170411271)
    3. Nurul Khasanah (5170411300)

---
+++ src/Program/UserID.java
@@ -0,0 +1,13 @@
+package Program;
+
+public class UserID {
+    private static String txtuser;
+ 
+public static void setUserLogin(String txtuser) {
+UserID.txtuser = txtuser;
+}
+public static String getUserLogin() {
+return txtuser;
+}
+
+}
